1674769032ea5d5bb04a8b13822dba45955d0697
fangwei2 <fangwei2@illinois.edu>
2019-03-10 03:17:15 -0500

    Updates

--- precache-manifest.1383ae0ddd548bbb364b208ac68cab40.js
+++ precache-manifest.1383ae0ddd548bbb364b208ac68cab40.js
@@ -56,20 +56,20 @@ self.__precacheManifest = [
     "url": "/CS498RK_MP2/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/CS498RK_MP2/static/media/icons.b87b9ba5.ttf"
-  },
-  {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/CS498RK_MP2/static/media/icons.8e3c7f55.eot"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/CS498RK_MP2/static/media/brand-icons.13db00b7.eot"
   },
   {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/CS498RK_MP2/static/media/icons.b87b9ba5.ttf"
+  },
+  {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/CS498RK_MP2/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/CS498RK_MP2/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
