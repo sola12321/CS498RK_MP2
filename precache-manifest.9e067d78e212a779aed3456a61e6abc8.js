25335803675de3cad1aef5b4902564e86e542dfb
fangwei2 <fangwei2@illinois.edu>
2019-03-11 07:07:00 -0500

    Updates

--- precache-manifest.9e067d78e212a779aed3456a61e6abc8.js
+++ precache-manifest.9e067d78e212a779aed3456a61e6abc8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ef46b4df428b4d6546fb",
+    "revision": "197d15d10dff51b43ef8",
     "url": "/CS498RK_MP2/static/css/main.9a4a3eb4.chunk.css"
   },
   {
-    "revision": "ef46b4df428b4d6546fb",
-    "url": "/CS498RK_MP2/static/js/main.ef46b4df.chunk.js"
+    "revision": "197d15d10dff51b43ef8",
+    "url": "/CS498RK_MP2/static/js/main.197d15d1.chunk.js"
   },
   {
     "revision": "984364fb488b5e78fbb4",
@@ -28,16 +28,16 @@ self.__precacheManifest = [
     "url": "/CS498RK_MP2/static/media/outline-icons.ef60a4f6.woff"
   },
   {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/CS498RK_MP2/static/media/flags.9c74e172.png"
+  },
+  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/CS498RK_MP2/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/CS498RK_MP2/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/CS498RK_MP2/static/media/flags.9c74e172.png"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/CS498RK_MP2/static/media/brand-icons.a046592b.woff"
   },
   {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/CS498RK_MP2/static/media/icons.8e3c7f55.eot"
+  },
+  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/CS498RK_MP2/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/CS498RK_MP2/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/CS498RK_MP2/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/CS498RK_MP2/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "d5679224a25f7034b9620a77b99e07f6",
+    "revision": "4c5c57436c842228f219e4296e179c69",
     "url": "/CS498RK_MP2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CS498RK_MP2/precache-manifest.bb8c4f61590aba406451909e2399e6c9.js"
+  "/CS498RK_MP2/precache-manifest.9e067d78e212a779aed3456a61e6abc8.js"
 );
 
 workbox.clientsClaim();
